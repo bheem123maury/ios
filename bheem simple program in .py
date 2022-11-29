d20e5ddb60c66d6e3ed310bf2c81b1421f615c3d
Bheem maurya <105773809+bheem123maury@users.noreply.github.com>
2022-11-29 14:38:41 +0530

    Add files via upload

---
+++ bheem simple program in .py
@@ -0,0 +1,11 @@
+""""
+vars="bheem"
+var1=45
+print("the number of ",str(vars)+str(var1))
+"""
+mystr="bheem  maurya is good boy"
+#print(mystr.replace("bheem","Ankit"))
+#print(len(mystr))
+#print(mystr[0:13])
+#print(mystr[::-1])
+print(mystr.isalpha)
